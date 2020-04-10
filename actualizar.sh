f83741ddad379678d6d7c976fd42639b48f67f5c
Manel <ea3eiz@ea3eiz.com>
2020-04-10 14:58:57 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -18,14 +18,6 @@ else
 echo ""
 fi
 
-#comprueba si el fichero existe
-if [ -f /home/pi/.local/sala.txt ];
-then
-echo "El fichero ya existe"
-else
-cp sala.txt /home/pi/.local
-fi
-
 # path usuario
 usuario="/home/pi"
 usuario="$usuario"
