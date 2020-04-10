01c3607cbb5015aa916b7362d7c99ab8bb058b00
Manel <ea3eiz@ea3eiz.com>
2020-04-10 20:54:24 +0200

    1

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo cp /home/pi/V105/Desktop/*.* /home/pi/Desktop
-sudo chmod 777 -R /home/pi/Desktop
 
-cp /home/pi/V105/icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
-xfdesktop --reload
+sudo cp -R /home/pi/.local/Desktop /home/pi
+
 
 
                                 
