eb6319941407915dbdcd1378746d6a693467b16e
Manel <ea3eiz@ea3eiz.com>
2020-04-10 17:30:57 +0200

    Update ejecutar_bm.sh

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -25,9 +25,9 @@ sed -i "1c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi/.local
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/.local/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/.local/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/.local/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi/.local
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
