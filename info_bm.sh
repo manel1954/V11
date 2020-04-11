8925891fcb3989f0eaf835508aeb1ddf719a542c
Manel <ea3eiz@ea3eiz.com>
2020-04-12 01:04:37 +0200

    1

--- info_bm.sh
+++ info_bm.sh
@@ -10,9 +10,24 @@ puerto="  "$puerto
 
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
 frecuencia=`expr substr $frecuencia 13 17`
-frecuencia=$frecuencia$puerto
+frecuencia=$frecuencia
 
-echo "Frecuencia: $frecuencia"
-echo "Puerto: $puerto"
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
+
+echo "${VERDE}"
+echo "******************************"
 echo ""
+echo "${CIAN}Frecuencia: ${AMARILLO}$frecuencia   "
+echo "${CIAN}Puerto: ${AMARILLO}$puerto   "
+echo ""
+echo "******************************"
+echo "${CIAN}"
+echo "Pulsa Enter para salir"
 read a
