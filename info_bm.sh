6dc1837a4cff565ff2e91303e9827ccc4465d314
Manel <ea3eiz@ea3eiz.com>
2020-04-12 00:37:33 +0200

    1

---
+++ info_bm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMBM.ini`
+buscar=":"
+caracteres=`expr index $mode $buscar`
+caracteres_linea=`expr $caracteres - 1`
+numero_linea_port=`expr substr $mode 1 $caracteres_linea`
+mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMBM.ini)
+puerto=`expr substr $mode 11 9`
+puerto="  "$puerto
+
+frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
+frecuencia=`expr substr $frecuencia 13 17`
+frecuencia=$frecuencia$puerto
+
+echo "Frecuencia: $frecuencia"
+echo "Puerto: $puerto"
