ff2624d9deb4923d9f431d7bfa4838d5a7654ed1
Manel <ea3eiz@ea3eiz.com>
2020-04-12 18:53:06 +0200

    1

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -2,22 +2,15 @@
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_MMDVMPLUS.desktop /home/pi
-sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
+cp Abrir_MMDVMPLUS.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
+cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
-
-sudo cp -R Desktop /home/pi/.local
-sleep 1
-sudo rm -R Desktop
-sudo cp -R /home/pi/.local/Desktop /home/pi
-sudo chmod 777 -R $usuario/Desktop
-
 
 cd /home/pi/MMDVMHost
 echo "\33[1;37m"
@@ -30,15 +23,10 @@ sudo ./MMDVMPLUS MMDVMPLUS.ini
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
 sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
-sudo sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
+cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
-
-sudo cp -R Desktop /home/pi/.local
-sleep 1
-sudo rm -R Desktop
-sudo cp -R /home/pi/.local/Desktop /home/pi
