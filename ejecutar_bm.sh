e37426419373c18955e8264f4c597a8e6e641708
Manel <ea3eiz@ea3eiz.com>
2020-04-12 18:44:26 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-
-
-
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_MMDVMBM.desktop /home/pi
+cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=ON" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
+cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
-
 
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
@@ -25,13 +21,13 @@ sleep 2
 sudo ./MMDVMBM MMDVMBM.ini
 
 cd /home/pi/Desktop
-sudo cp Abrir_MMDVMBM.desktop /home/pi
+cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
 cd /home/pi
-sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
+cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
 
