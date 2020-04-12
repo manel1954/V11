8a6a1ae23f2930db668ca447509a505378fa5e83
Manel <ea3eiz@ea3eiz.com>
2020-04-12 11:38:50 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
-mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMBM.ini`
-buscar=":"
-caracteres=`expr index $mode $buscar`
-caracteres_linea=`expr $caracteres - 1`
-numero_linea_port=`expr substr $mode 1 $caracteres_linea`
-mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMBM.ini)
-puerto=`expr substr $mode 11 9`
-puerto="  "$puerto
-cd /home/pi/Desktop
-sudo cp RXF_BM.desktop /home/pi
 
-frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMBM.ini)
-frecuencia=`expr substr $frecuencia 13 17`
-frecuencia=$frecuencia$puerto
-sed -i "11c Name=$frecuencia" /home/pi/RXF_BM.desktop
 
-sudo cp /home/pi/RXF_BM.desktop /home/pi/Desktop
 
-sudo rm /home/pi/RXF_BM.desktop
-
-#Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                        
-sed -i "1c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
