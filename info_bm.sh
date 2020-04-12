fa7f3600833f4d96ead0a81b3a5875adee0fbd60
Manel <ea3eiz@ea3eiz.com>
2020-04-12 12:47:57 +0200

    1

--- info_bm.sh
+++ info_bm.sh
@@ -18,7 +18,7 @@ caracteres=`expr index $address $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_address=`expr substr $address 1 $caracteres_linea`
 mode=$(awk "NR==$numero_linea_address" /home/pi/MMDVMHost/MMDVMBM.ini)
-address=`expr substr $address 13 20`
+address=`expr substr $address 13 30`
 address="  "$address
 
 #Colores
