1b5037f7e79d652c19a48d3d100aec73de90c400
Manel <ea3eiz@ea3eiz.com>
2020-04-12 18:56:20 +0200

    1

--- cerrar_DMRPLUS.sh
+++ cerrar_DMRPLUS.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo killall MMDVMPLUS
 cd /home/pi/Desktop
-sudo cp Abrir_MMDVMPLUS.desktop /home/pi
+cp Abrir_MMDVMPLUS.desktop /home/pi
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
 
 cd /home/pi
-sudo cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
+cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMPLUS.desktop
 
--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -5,7 +5,7 @@ cd /home/pi/Desktop
 cp Abrir_MMDVMPLUS.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
 cd /home/pi
 cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
@@ -24,7 +24,7 @@ cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
 sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
+sed -i "10c Name[es_ES]=DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
 cd /home/pi
 cp Abrir_MMDVMPLUS.desktop /home/pi/Desktop
