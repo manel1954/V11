a091e31798ecb34838cba3710b13f139e8989189
Manel <ea3eiz@ea3eiz.com>
2020-04-13 17:41:43 +0200

    1

--- Desktop/cerrar_dmrgateway.sh
+++ Desktop/cerrar_dmrgateway.sh
@@ -8,7 +8,7 @@ sleep 1
 sed -i "4c Exec=sh -c 'cd /home/pi/V11; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/V11/Abrir_dmrgateway.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ABRIR.png" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=Open DMRGateway" /home/pi/Abrir_dmrgateway.desktop
-sed -i "5c DMRGateway=OFF" /home/repeater/status.ini
+sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
 sleep 1
 cd /home/pi/
 sudo cp Abrir_dmrgateway.desktop /home/pi/Desktop
