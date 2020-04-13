c647dd5e78a30fcee0d1f86abd68e7d5b488c45b
Manel <ea3eiz@ea3eiz.com>
2020-04-13 11:38:29 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -6,7 +6,7 @@ cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_CERRAR.png" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=BM" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "7c MMDVMBM=ON" /home/pi/.local
+sed -i "7c MMDVMBM=ON" /home/pi/.local/status.ini
 cd /home/pi
 cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
