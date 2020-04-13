5cfe47950698c721e263151a853298670c9b4770
Manel <ea3eiz@ea3eiz.com>
2020-04-14 00:36:28 +0200

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 sudo killall MMDVMBM
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-sudo cp Abrir_MMDVMBM.desktop /home/pi
 
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
-sleep 10
-sed -i "10c Name[es_ES]=abriri BM" /home/pi/Abrir_MMDVMBM.desktop
+
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=abriri BM" /home/pi/Desktop/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/.local/status.ini
 
-cd /home/pi
-sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
-
-sudo rm /home/pi/Abrir_MMDVMBM.desktop
+p
 
 
 
--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-cp Abrir_MMDVMBM.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sleep 10
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_CERRAR.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=CERRAR BM" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "7c MMDVMBM=ON" /home/pi/.local/status.ini
-cd /home/pi
-cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
-sudo rm /home/pi/Abrir_MMDVMBM.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_bm.sh'" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_CERRAR.png" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=CERRAR BM" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sed -i "7c MMDVMBM=ON" /home/pi/.local/status.ini
+
 
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
