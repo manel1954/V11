4886caee0ce664b2158c04d9058d018219a27f6b
Manel <ea3eiz@ea3eiz.com>
2020-04-13 17:35:10 +0200

    1

--- Desktop/cerrar_dmrgateway.sh
+++ Desktop/cerrar_dmrgateway.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+sudo killall MMDVMDMRGATEWAY
+sudo killall DMRGateway
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-
-cd /home/repeater/Desktop
-sudo cp Abrir_DMRGateway.desktop /home/repeater/
+cd /home/pi/Desktop
+sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/repeater/$SCRIPTS_version; lxterminal --geometry=72x15 -e sudo sh ejecutar_DMRGateway_V8.sh'" /home/repeater/Abrir_DMRGateway.desktop
-sed -i "5c Icon=/home/repeater/$SCRIPTS_version/DMRGateway_OFF.png" /home/repeater/Abrir_DMRGateway.desktop
-sed -i "10c Name[es_ES]=Open DMRGateway" /home/repeater/Abrir_DMRGateway.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/V11; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/V11/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ABRIR.png" /home/pi/Abrir_dmrgateway.desktop
+sed -i "10c Name[es_ES]=Open DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "5c DMRGateway=OFF" /home/repeater/status.ini
 sleep 1
-cd /home/repeater/
-sudo cp Abrir_DMRGateway.desktop /home/repeater/Desktop
+cd /home/pi/
+sudo cp Abrir_dmrgateway.desktop /home/pi/Desktop
 sleep 1
-sudo rm /home/repeater/Abrir_DMRGateway.desktop
-sudo killall MMDVMHost &
-sudo killall DMRGateway
+sudo rm /home/pi/Abrir_dmrgateway.desktop
