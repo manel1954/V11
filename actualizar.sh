c7832d1ceb4f66b459e871857d1184486a893c4b
Manel <ea3eiz@ea3eiz.com>
2020-04-14 12:33:47 +0200

    1

--- actualizar.sh
+++ actualizar.sh
@@ -42,7 +42,7 @@ fi
 
 
 SCRIPTS_version="V11"
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Desktop/Abrir_MMDVMBM.desktop
+sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/V11/Desktop/Abrir_MMDVMBM.desktop
 
 
 
