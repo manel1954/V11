aac65d065e36b317df38e3b747a8aab74c8ed88a
Manel <ea3eiz@ea3eiz.com>
2020-04-16 16:22:34 +0200

    1

--- idioma.sh
+++ idioma.sh
@@ -59,6 +59,8 @@ read -p '   Quieres cambiar de idioma? S/N ' actualizar
                         sed -i "1c Spanish" /home/pi/.local/idioma
                         sudo sed -i "5c Icon=/home/pi/V11/ICONO_SPANISH.png" /home/pi/V11/Desktop/Idioma.desktop
                         sudo sed -i "11c Name=Editar BM" /home/pi/V11/Desktop/Editar_MMDVMBM.desktop
+                        sudo sed -i "11c Name[es_ES]=SOLO FUSION" /home/pi/V11/Desktop/Abrir_solofusion.desktop
+                        sudo sed -i "11c Name[es_ES]=SOLO DSTAR" /home/pi/V11/Desktop/Abrir_solodstar.desktop
                         sudo rm -R /home/pi/Desktop
                         sleep 1
                         cp -R /home/pi/.local/Desktop /home/pi
@@ -93,8 +95,8 @@ read -p '   You want to change language? Y/N ' actualizar
                         sed -i "1c English" /home/pi/.local/idioma
                         sudo sed -i "5c Icon=/home/pi/V11/ICONO_ENGLISH.png" /home/pi/V11/Desktop/Idioma.desktop
                         sudo sed -i "11c Name=Edit BM" /home/pi/V11/Desktop/Editar_MMDVMBM.desktop
-                        sudo sed -i "11c Name=ONLY FUSION" /home/pi/V11/Desktop/Abrir_solofusion.desktop
-                        sudo sed -i "11c Name=ONLY DSTAR" /home/pi/V11/Desktop/Abrir_solodstar.desktop
+                        sudo sed -i "11c Name[es_ES]=ONLY FUSION" /home/pi/V11/Desktop/Abrir_solofusion.desktop
+                        sudo sed -i "11c Name[es_ES]=ONLY DSTAR" /home/pi/V11/Desktop/Abrir_solodstar.desktop
                         cp -R /home/pi/Desktop /home/pi/.local
                         sudo rm -R /home/pi/Desktop
                         sleep 1
