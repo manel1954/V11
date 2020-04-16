e4eec2651a82dc56b32a17e11a7247d951fb69c2
Manel <ea3eiz@ea3eiz.com>
2020-04-16 18:41:00 +0200

    1

--- idioma.sh
+++ idioma.sh
@@ -96,7 +96,7 @@ read -p '   You want to change language? Y/N ' actualizar
                         sudo sed -i "5c Icon=/home/pi/V11/ICONO_ENGLISH.png" /home/pi/V11/Desktop/Idioma.desktop
                         sudo sed -i "11c Name=Edit BM" /home/pi/V11/Desktop/Editar_MMDVMBM.desktop
                         sudo sed -i "11c Name[es_ES]=ONLY FUSION" /home/pi/V11/Desktop/Abrir_solofusion.desktop
-                        sudo sed -i "11c Name[es_ES]=ONLY  DSTAR" /home/pi/V11/Desktop/Abrir_solodstar.desktop
+                        sudo sed -i "11c Name[es_ES]=ONLY  xDSTAR" /home/pi/V11/Desktop/Abrir_solodstar.desktop
                         cp -R /home/pi/Desktop /home/pi/.local
                         sudo rm -R /home/pi/Desktop
                         sleep 1
