6c29f90110d7ceb25c167ba719b9b1c954b32cd3
Manel <ea3eiz@ea3eiz.com>
2020-04-16 23:50:52 +0200

    1

--- cerrar_bm.sh
+++ cerrar_bm.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+idioma=$(awk "NR==1" /home/pi/.local/idioma)
+if [ $idioma = English ]; then
+icono=ICONO_OPEN.png
+ 
+else
+icono=ICONO_ABRIR.png
+fi
+
 sudo killall MMDVMBM
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
 
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/.local/status.ini
 
@@ -13,6 +21,7 @@ cd /home/pi
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
+
 
 
 
--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -2,27 +2,10 @@
 idioma=$(awk "NR==1" /home/pi/.local/idioma)
 if [ $idioma = English ]; then
 icono=ICONO_CLOSE.png
-OPTION="Chose an Option"
-PUERTO="Port for DVMEGA punctured in Raspberry PI"
-INDICATIVO="Enter your call sign"
-VALORINDICATIVO="Current value call sign"
-SALIR="Exit"
-RESTAURAORIGINAL="You want to restore the original file"
-SINO="Y/N"
-P_RESTAURAORIGINAL="Recover the original file"
-MODIFICAR="Modify   "
- 
+
 else
 icono=ICONO_CERRAR.png
-OPTION="Elije una opción"
-PUERTO="Puerto para DVMEGA pinchado en Raspberri PI"
-INDICATIVO="Introduce tu indicativo"
-VALORINDICATIVO="Valor actual Indicativo"
-SALIR="Salir"
-RESTAURAORIGINAL="Quieres restaurar el fichero original"
-SINO="S/N"
-P_RESTAURAORIGINAL=" Recuperar fichero original"
-MODIFICAR="Modificar"
+
 fi
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
@@ -47,7 +30,7 @@ sudo ./MMDVMBM MMDVMBM.ini
 cd /home/pi/Desktop
 cp Abrir_MMDVMBM.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15 -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ABRIR.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "10c Name[es_ES]=BM" /home/pi/Abrir_MMDVMBM.desktop
 sed -i "7c MMDVMBM=OFF" /home/pi/.local/status.ini
 cd /home/pi
