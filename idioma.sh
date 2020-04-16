c06ce53463b3007dc35861c479413a5b59a4ba0a
Manel <ea3eiz@ea3eiz.com>
2020-04-16 12:56:17 +0200

    1

--- idioma.sh
+++ idioma.sh
@@ -91,6 +91,7 @@ read -p '   You want to change language? Y/N ' actualizar
                         sed -i "1c English" /home/pi/.local/idioma
                         sudo sed -i "5c Icon=/home/pi/V11/ICONO_ENGLISH.png" /home/pi/V11/Desktop/Idioma.desktop
                         sudo sed -i "11c Name=Edit BM" /home/pi/V11/Desktop/Editar_MMDVMBM.desktop
+                        sudo sed -i "13c Name=Edit BM" /home/pi/V11/Desktop/Editar_MMDVMBM.desktop
                         sudo reboot
                         break;;
                         [nN]* ) echo ""
