0c910bbb90d402c5fdc9598f1f8332de76a2e8e3
Manel <ea3eiz@ea3eiz.com>
2020-04-17 11:49:37 +0200

    1

--- cerrar_ESPECIAL.sh
+++ cerrar_ESPECIAL.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 sudo killall MMDVMESPECIAL
+
+idioma=$(awk "NR==1" /home/pi/.local/idioma)
+if [ $idioma = English ]; then
+icono=ICONO_OPEN.png
+ 
+else
+icono=ICONO_ABRIR.png
+fi
+
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
-#sleep 1
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+
 sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=80x15 -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=OFF" /home/pi/.local/status.ini
-#sleep 1
+
 cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
-#sleep 1
+
 sudo rm /home/pi/Abrir_ESPECIAL.desktop
+
 
