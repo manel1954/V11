a93a80e4de8cb28da37d736252b9b27326f753f3
Manel <ea3eiz@ea3eiz.com>
2020-04-18 00:26:13 +0200

    1

--- cerrar_solodstar.sh
+++ cerrar_solodstar.sh
@@ -20,9 +20,12 @@ sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=80x15
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_solodstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo DSTAR" /home/pi/Abrir_solodstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/pi/.local/status.ini
+
+
+sleep 20
+
 cd /home/pi
 sudo cp Abrir_solodstar.desktop /home/pi/Desktop
-read a
 sudo rm /home/pi/Abrir_solodstar.desktop
 
 
