2a3ee45575445e5da888f933acfedfd285ac3ea0
Manel <ea3eiz@ea3eiz.com>
2020-04-17 16:27:44 +0200

    1

--- cerrar_dmrgateway.sh
+++ cerrar_dmrgateway.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 sudo killall MMDVMDMRGATEWAY
 sudo killall DMRGateway
+
+idioma=$(awk "NR==1" /home/pi/.local/idioma)
+if [ $idioma = English ]; then
+icono=ICONO_OPEN.png
+ 
+else
+icono=ICONO_ABRIR.png
+fi
+
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/pi/V11; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ABRIR.png" /home/pi/Abrir_dmrgateway.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=Open DMRGateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
 sleep 1
@@ -14,3 +23,4 @@ cd /home/pi/
 sudo cp Abrir_dmrgateway.desktop /home/pi/Desktop
 sleep 1
 sudo rm /home/pi/Abrir_dmrgateway.desktop
+
--- ejecutar_dmrgateway.sh
+++ ejecutar_dmrgateway.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+idioma=$(awk "NR==1" /home/pi/.local/idioma)
+if [ $idioma = English ]; then
+icono=ICONO_CLOSE.png
+icono_NO=ICONO_OPEN.png
+
+else
+icono=ICONO_CERRAR.png
+icono_NO=ICONO_ABRIR.png
+
+fi
+
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+
 #Colores
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
@@ -9,8 +22,8 @@ GRIS="\033[0m"
 cd /home/pi/Desktop
 sudo cp Abrir_dmrgateway.desktop /home/pi/
 sleep 1
-sed -i "4c Exec=sh -c 'cd /home/pi/V11; sudo sh cerrar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
-sed -i "5c Icon=/home/pi/V11/ICONO_CERRAR.png" /home/pi/Abrir_dmrgateway.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono" /home/pi/Abrir_dmrgateway.desktop
 sed -i "10c Name[es_ES]=DMR Gateway" /home/pi/Abrir_dmrgateway.desktop
 sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
 sleep 1
@@ -35,3 +48,17 @@ sleep 2
 
 cd /home/pi/MMDVMHost
 sudo ./MMDVMDMRGATEWAY DMRGateway.ini 
+
+# Cierra el icono Abrir Solo Dstar si no hay conexión
+cd /home/pi/Desktop
+sudo cp Abrir_dmrgateway.desktop /home/pi/
+sleep 1
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=72x20 -e sudo sh ejecutar_dmrgateway.sh'" /home/pi/Abrir_dmrgateway.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/$icono_NO" /home/pi/Abrir_dmrgateway.desktop
+sed -i "10c Name[es_ES]=Open DMRGateway" /home/pi/Abrir_dmrgateway.desktop
+sed -i "19c DMRGateway=OFF" /home/pi/.local/status.ini
+sleep 1
+cd /home/pi/
+sudo cp Abrir_dmrgateway.desktop /home/pi/Desktop
+sleep 1
+sudo rm /home/pi/Abrir_dmrgateway.desktop
