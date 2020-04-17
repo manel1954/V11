f8f469a3e6c0c4b12f8987c907ea75e0381bac4f
Manel <ea3eiz@ea3eiz.com>
2020-04-17 16:33:43 +0200

    1

--- idioma.sh
+++ idioma.sh
@@ -94,10 +94,7 @@ read -p '   Quieres cambiar de idioma? S/N ' actualizar
                         sudo sed -i "10c Name[es_ES]=Activar DVSWITCH" /home/pi/V11/Desktop/Activar_dvswitch.desktop
                         sudo sed -i "11c Name[es_ES]=Nota Versión" /home/pi/V11/Desktop/Nota_version.desktop
                         sudo sed -i "10c Name[es_ES]=Editor General" /home/pi/V11/Desktop/Editar_General.desktop
-                        
-
-
-
+                        sudo sed -i "11c Name[es_ES]=Editar DMRGateway" /home/pi/V11/Desktop/Editar_dmrgateway.desktop
                         sudo reboot
                         break;;
                         [nN]* ) echo ""
@@ -163,6 +160,7 @@ read -p '   You want to change language? Y/N ' actualizar
                         sudo sed -i "10c Name[es_ES]=Activate DVSWITCH" /home/pi/V11/Desktop/Activar_dvswitch.desktop
                         sudo sed -i "11c Name[es_ES]=Note Version" /home/pi/V11/Desktop/Nota_version.desktop
                         sudo sed -i "10c Name[es_ES]=General Editor" /home/pi/V11/Desktop/Editar_General.desktop
+                        sudo sed -i "11c Name[es_ES]=Edit DMRGateway" /home/pi/V11/Desktop/Editar_dmrgateway.desktop
                         sudo reboot
                         break;;
                         [nN]* ) echo ""
