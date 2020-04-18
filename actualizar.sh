b4473a9a710a7ba56687bb323a72f1fdfa9af27d
Manel <ea3eiz@ea3eiz.com>
2020-04-18 16:47:10 +0200

    1

--- actualizar.sh
+++ actualizar.sh
@@ -103,7 +103,19 @@ cd $usuario/$SCRIPTS_version/Desktop
 cp * $usuario/Desktop
 sudo chmod 777 -R $usuario/Desktop
 
+estado_dvswitch=$(awk "NR==18" /home/pi/.local/status.ini)
+if [ "$estado_dvswitch" = 'DVSWITCH=OFF' ];then
+sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NEXTIONDRIVER_OFF.png" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
+else
+sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NEXTIONDRIVER_ON.png" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
+fi
 
+idioma=$(awk "NR==1" /home/pi/.local/idioma)
+if [ $idioma = English ]; then
+sudo sed -i "10c Name[es_ES]=Activate NextionDri" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
+else
+sudo sed -i "10c Name[es_ES]=Activar NextionDri" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
+fi
 
 # excepto estos que tienen que mantener su status
 # sudo cp $usuario/.local/Activar_dvswitch.desktop $usuario/Desktop #deja el icono en el estado que se reinició
@@ -335,19 +347,7 @@ sudo rm -R /home/pi/Desktop
 sleep 1
 cp -R /home/pi/.local/Desktop /home/pi
 
-estado_dvswitch=$(awk "NR==18" /home/pi/.local/status.ini)
-if [ "$estado_dvswitch" = 'DVSWITCH=OFF' ];then
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NEXTIONDRIVER_OFF.png" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
-else
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NEXTIONDRIVER_ON.png" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
-fi
 
-idioma=$(awk "NR==1" /home/pi/.local/idioma)
-if [ $idioma = English ]; then
-sudo sed -i "10c Name[es_ES]=Activate NextionDri" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
-else
-sudo sed -i "10c Name[es_ES]=Activar NextionDri" /home/pi/V11/Desktop/Activar_NextionDriver.desktop
-fi
 
 
 
